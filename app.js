a2d15a19bfac4a42b699e0d46ea1eea10c5ddbe0
rishikesh835 <rishikeshmishra835@gmail.com>
2024-01-23 20:58:35 +0530

    database details hidden

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
-const express=require("express")
-const path=require("path")
-const fs=require("fs")
-const app=express()
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+const bodyParser=require("body-parser")
+const mongoose = require('mongoose');
+var dotenv = require('dotenv');
+
+const app = express()
 const hostname = '127.0.0.1'
 const port = 8000;
+
+
+dotenv.config();
+var url = process.env.MONGOLAB_URL;
+
+
+
+async function main() {
+  
+  await mongoose.connect(url);
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+//mongoose and mongoDB STUFF
+main().catch(err => console.log(err));
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  phone: Number,
+  email: String,
+  address: String,
+  desc: String
+});
+
+var Contact = mongoose.model('Contact',contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
@@ -14,18 +43,26 @@ app.set('view engine', 'pug') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
 
 // ENDPOINTS
-app.get('/', (req, res)=>{
-    
-    // const params = {}
-    res.status(200).render('home.pug');
+app.get('/', (req, res) => {
+
+  // const params = {}
+  res.status(200).render('home.pug');
 })
 //params is object which contains variable  for pug file 
-app.get('/', (req, res)=>{
-    
-    const params = {}
-    res.status(200).render('contact.pug', params);
-})
+app.get('/contact', (req, res) => {
 
+  
+  res.status(200).render('contact.pug');
+
+})
+app.post('/contact',(req,res)=>{
+  var myData=new Contact(req.body);
+  myData.save().then(()=>{
+    res.send("this item is sent to datababse")
+  }).catch(()=>{
+    res.status(400).send("item was not saved to the database")
+  })
+})
 
 
 
@@ -36,35 +73,6 @@ app.get('/', (req, res)=>{
 
 //starting the server
 app.listen(port, () => {
-    console.log(`The application has started on port http://${hostname}:${port}/`);
+  
+  console.log(`The application has started on port http://${hostname}:${port}/`);
 })
-
-
-//mongoose stuff
-// getting-started.js
-const mongoose = require('mongoose');
-
-main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb+srv://rishikeshmishra835:qulsIrUBxgMKR4i4@cluster0.mon7jy4.mongodb.net/tutorial2?retryWrites=true&w=majority');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
-//new schema declared down below
-const phoneSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    rating: Number,
-    qty: Number,
-    sold: Number  
-
-  });
-
-//creating model
-const phone= mongoose.model('phone',phoneSchema)
-const realme = new phone({name:"realme 9pro", price: 17000, rating:4.5,qty: 500, sold:655 })
-
-realme.save();
-
-
